491dad1859ba7ab791699012a9caa470e44b4605
admin <admin@host.docker.internal>
2021-10-12 10:45:01 +0900

    ex9no13

---
+++ ex9no13/src/ex9no13/ex9no13.java
@@ -0,0 +1,12 @@
+package ex9no13;
+
+public class ex9no13 {  //  468p
+	public static void main(String[] args) {
+		char[] cArr = new char[0];
+		String s = new String(cArr);
+		
+		System.out.println("cArr.length = "+ cArr.length);
+		System.out.println("@@@"+s+"@@@");
+	}
+
+}
